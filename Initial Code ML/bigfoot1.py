262358a0ece417f862f6a6ef05f4851d7c822705
Rebecca Tast <69356535+rebeccatast@users.noreply.github.com>
2021-03-27 11:52:39 -0500

    Create bigfoot1.py

---
+++ Initial Code ML/bigfoot1.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+# In[9]:
+
+
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D, MaxPool2D, Flatten, Dropout
+from keras.preprocessing.image import ImageDataGenerator
+from keras.optimizers import Adam
+
+
+# In[10]:
+
+
+pip install scikit-learn
+
+
+# In[11]:
+
+
+from sklearn.metrics import classification_report, confusion_matrix
+
+
+# In[12]:
+
+
+import tensorflow as tf
+
+
+# In[15]:
+
+
+import opencv
+import os
+import numpy as np
+
+
+# In[14]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
